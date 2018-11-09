17b40a7e36e51d140f0918aca46b2c3086431eb1
andrewekhalel <andrew_emel@hotmail.com>
2018-11-09 17:43:46 +0100

    Update tests

---
+++ edafa/tests/tests.py
@@ -0,0 +1,118 @@
+from unittest import TestCase
+from edafa import ClassPredictor
+import os
+from abc import abstractmethod
+import warnings
+
+class Child(ClassPredictor):
+	"""
+	Child class to be used in testing
+	"""
+	def __init__(self,*args,**kwargs):
+		super().__init__(*args,**kwargs)
+
+	def predict_patches(self,patches):
+		pass
+
+class Tester(TestCase):
+	def __init__(self, *args, **kwargs):
+		super(Tester, self).__init__(*args, **kwargs)
+		self.path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+
+	def test_pass_json_file(self):
+		"""
+		Test configuration file loading
+		"""
+		p = Child(os.path.join(self.path,"conf/pascal_voc.json"))
+		self.assertTrue(p.augs == ["NO",
+									"FLIP_UD",
+									"FLIP_LR"])
+		self.assertTrue(p.mean == "ARITH")
+		self.assertTrue(p.bits == 8)
+
+	def test_pass_json_string(self):
+		"""
+		Test configuration as string
+		"""
+		conf = '{"augs":["NO",\
+				"FLIP_UD",\
+				"FLIP_LR"],\
+				"mean":"ARITH",\
+				"bits":8}'
+		p = Child(conf)
+		self.assertTrue(p.augs == ["NO",
+									"FLIP_UD",
+									"FLIP_LR"])
+		self.assertTrue(p.mean == "ARITH")
+		self.assertTrue(p.bits == 8)
+
+	def test_pass_no_augs(self):
+		"""
+		Test configuration without augs
+		"""
+		conf = '{"mean":"ARITH",\
+				"bits":8}'
+		p = Child(conf)
+			
+		self.assertTrue(p.augs == ["NO"])
+		self.assertTrue(p.mean == "ARITH")
+		self.assertTrue(p.bits == 8)
+		self.assertRaises(SyntaxWarning)
+
+	def test_pass_no_mean(self):
+		"""
+		Test configuration without mean
+		"""
+		conf = '{"augs":["NO"],\
+				"bits":8}'
+	
+		with self.assertWarns(SyntaxWarning):
+			p = Child(conf)
+			
+		self.assertTrue(p.augs == ["NO"])
+		self.assertTrue(p.mean == "ARITH")
+		self.assertTrue(p.bits == 8)
+
+	def test_pass_no_bits(self):
+		"""
+		Test configuration without bits
+		"""
+		conf = '{"augs":["NO"],\
+				"mean":"ARITH"}'
+			
+		with self.assertWarns(SyntaxWarning):
+			p = Child(conf)
+
+		self.assertTrue(p.augs == ["NO"])
+		self.assertTrue(p.mean == "ARITH")
+		self.assertTrue(p.bits == 8)
+
+	def test_pass_invalid_augs(self):
+		"""
+		Test configuration invalid augs
+		"""
+		conf = '{"augs":["INVALID"],\
+				"mean":"ARITH",\
+				"bits":8}'
+		
+		# TODO: replace Exception with AugmentationUnrecognized
+		with self.assertRaises(Exception):
+			p = Child(conf)
+
+	def test_pass_invalid_mean(self):
+		"""
+		Test configuration invalid mean
+		"""
+		conf = '{"augs":["NO"],\
+				"mean":"INVALID",\
+				"bits":8}'
+
+		# TODO: replace Exception with MeanUnrecognized
+		with self.assertRaises(Exception):
+			p = Child(conf)
+
+		
+
+
+if __name__ == '__main__':
+    unittest.main()
